5892f6fe1fbcb53baaaf4d4f6d3696883ed8415c
Reis-C <carlareisaus@gmail.com>
2021-09-19 19:40:00 -0300

    aula 37 heranca intro

---
+++ src/com/carla/cursojava/aula37_heranca/Pessoa.java
@@ -0,0 +1,52 @@
+package com.carla.cursojava.aula37_heranca;
+
+public class Pessoa {
+
+    //private = outros classes nao terao acesso. acesso apenas aos metodos
+    private String nome;
+    private String endereco;
+    private String telefone;
+    private String cpf;
+
+    private String telefoneCelular;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefoneCelular() {
+        return telefoneCelular;
+    }
+
+    public void setTelefoneCelular(String telefoneCelular) {
+        this.telefoneCelular = telefoneCelular;
+    }
+}
---
+++ src/com/carla/cursojava/aula37_heranca/Teste.java
@@ -0,0 +1,26 @@
+package com.carla.cursojava.aula37_heranca;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        /*Aluno aluno = new Aluno();
+
+        Professor professor = new Professor();
+
+        Pessoa pessoa = new Pessoa();
+
+        aluno.setNome("Maria");*/
+
+        //professor.setTelefoneCelular("----");
+
+        //Pessoa = superclasse
+        Pessoa aluno = new Aluno();
+        //aluno.get
+
+        //disponivel apenas os metodos da classe Pessoa
+        Pessoa professor = new Professor();
+        //professor.getSala
+
+    }
+}
